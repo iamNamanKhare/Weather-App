bb2a50bce6f3601bc02677e2c1c61f90f47ecb72
iamNamanKhare <namankhare42@gmail.com>
2020-05-30 21:16:08 +0530

    UI Improvement

--- public/script.js
+++ public/script.js
@@ -1,10 +1,12 @@
 function fillData(obj){
 
+    let tempCard = $('#tempCard')
     let cardTitle = $('#cardTitle')
     let cardMain = $('#cardMain')
     let cardTemp = $('#cardTemp')
     let cardTempMinMax = $('#cardTempMinMax')
     let cardDesc = $('#cardDesc')
+    let rainBox = $('#rainBox')
 
     let temp = (obj.main.temp - 273.15).toFixed(1)
     let tempMin = (obj.main.temp_min - 273.15).toFixed(1)
@@ -22,12 +24,28 @@ function fillData(obj){
     cardTemp.html(`<b>Temp:</b> ${temp} <br><b>Feels Like :</b> ${tempFeelsLike}`)
     cardTempMinMax.html(`<b>Temp Min :</b> ${tempMin} <br> <b>Temp Max :</b> ${tempMax}`)
     cardDesc.html(`<b>Description : </b> ${obj.weather[0].description}`)
+
+    if(obj.weather[0].main.includes('rain') || obj.weather[0].main.includes('Rain')){
+        rainBox.html(`So, is it Raining ? : Yup. Enjoy Reading a Book with Coffee :)`)
+    }
+    else{
+        rainBox.html(`So, is it Raining ? : Nope :)`)
+    }
+
+    rainBox.show()
+    tempCard.show()
 }
 
 $(function(){
+    
     let findTemp = $('#tempButton')
     let cityField = $('#cityName')
+    let rainBox = $('#rainBox')
+    let tempCard = $('#tempCard')
 
+    rainBox.hide()
+    tempCard.hide()
+    
     findTemp.click(() => {
         let cityName = cityField.val()
 
